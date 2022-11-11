e5d7d928189b0d67df7a221ac4cf733ffb1c6c5f
Clara <clarapachecodev@gmail.com>
2022-11-11 15:23:49 -0300

    Create function to capture initial size of the screen

--- game.js
+++ game.js
@@ -1,15 +1,23 @@
 
-let screenHeight 
-let screenWidth 
+let screenHeight = 0
+let screenWidth = 0
+
+function ajustaTamanhoPalcoJogo(){
+  screenHeight = window.innerHeight
+  screenWidth = window.innerWidth
+  console.log(screenWidth,screenHeight)
+  
+}
+
+ajustaTamanhoPalcoJogo()
 
 window.addEventListener('resize', function(){
   screenHeight = window.innerHeight
   screenWidth = window.innerWidth
-  
+  console.log(screenWidth, screenHeight)
 })
 
-console.log(screenHeight,screenWidth)
 
-
-
+let posicaoX = Math.random()
+let posicaoY = Math.random()
 
