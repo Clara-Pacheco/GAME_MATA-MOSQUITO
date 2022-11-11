e31827e51a670b2016b12b458cedc6050bdd2fac
Clara <clarapachecodev@gmail.com>
2022-11-11 14:53:39 -0300

    Change js file name and add logic to capture game´s stage size dynamically

---
+++ game.js
@@ -0,0 +1,15 @@
+
+let screenHeight 
+let screenWidth 
+
+window.addEventListener('resize', function(){
+  screenHeight = window.innerHeight
+  screenWidth = window.innerWidth
+  
+})
+
+console.log(screenHeight,screenWidth)
+
+
+
+
