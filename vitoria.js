3f748da7e9cf6ff6e89468b072803c24c0674d4d
Clara <clarapachecodev@gmail.com>
2022-11-14 10:24:11 -0300

    Redirect to index.html when click on 'Reiniciar' button

--- vitoria.js
+++ vitoria.js
@@ -1,5 +1,5 @@
 const restartButton = document.querySelector('[type= "button"]')
 restartButton.addEventListener('click', function(){
- window.location.href='game.html'
+ window.location.href='index.html'
 
 })
