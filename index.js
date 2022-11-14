0f36b67b9d13ae7b67dc17f914d9a48df91e0530
Clara <clarapachecodev@gmail.com>
2022-11-14 07:20:18 -0300

    Move scripts to 'vitoria' and 'index' js files

---
+++ index.js
@@ -0,0 +1,13 @@
+const startGame = document.querySelector('#startGame')
+startGame.addEventListener('click', function(){
+ let valueSelect = document.querySelector('#selectLevel').value
+ if(valueSelect === ''){
+  alert('Selecione um nível para iniciar o jogo')
+  return false
+ }else{
+  alert(valueSelect)
+ }
+
+})
+
+
